7c9fd5499632b5506528b5351b22dd6c182fc85b
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-19 21:52:10 -0600

    installed tailwind suceesfully

---
+++ src/components/inxel-test.tsx
@@ -0,0 +1,229 @@
+import React from "react";
+import { useState } from "react";
+
+function InxelTest() {
+  const [typeDisplay, setTypeDisplay] = useState("person");
+  const [isSorted, setIsSorted] = useState(false);
+  const [inputForm, setInputData] = useState({
+    name: "",
+    number: "",
+    type: "person",
+  });
+  const [data, setData] = useState([
+    {
+      name: "Juan",
+      number: "1209312",
+      type: "person",
+    },
+    {
+      name: "Pedro",
+      number: "901031",
+      type: "person",
+    },
+    {
+      name: "Ricardo",
+      number: "82832",
+      type: "company",
+    },
+    {
+      name: "Fernando",
+      number: "1291203",
+      type: "company",
+    },
+  ]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInputData({ ...inputForm, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newEntry = { ...inputForm };
+    if (inputForm.name || inputForm.number !== "") {
+      setData((prevData) => [...prevData, newEntry]);
+      setInputData({
+        name: "",
+        number: "",
+        type: "person",
+      });
+    } else null;
+  };
+
+  const handleDelete = (id) => {
+    setData((prevData) => prevData.filter((card) => card.number !== id));
+  };
+
+  const handleAlphabetic = () => {
+    setIsSorted(!isSorted);
+  };
+
+  const filteredPerson = data.filter((item) => {
+    return item.type === "person";
+  });
+  const filteredCompany = data.filter((item) => {
+    return item.type === "company";
+  });
+
+  const sortedPersonArray = [...filteredPerson].sort((a, b) => a.name.localeCompare(b.name));
+
+  const sortedCompanyArray = [...filteredCompany].sort((a, b) => a.name.localeCompare(b.name));
+
+  return (
+    <article style={{ display: "flex", borderRadius: "10px", border: "1px solid white" }}>
+      <div
+        style={{
+          width: "200px",
+          height: "300px",
+          boxSizing: "border-box",
+          padding: "10px",
+        }}
+      >
+        <form style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+          <input
+            name="name"
+            value={inputForm.name}
+            onChange={handleChange}
+            placeholder="name"
+            type="text"
+            style={{ height: "40px", borderRadius: "10px", border: "none", paddingLeft: "5px" }}
+          ></input>
+          <input
+            name="number"
+            value={inputForm.number}
+            onChange={handleChange}
+            placeholder="number"
+            type="text"
+            style={{ height: "40px", borderRadius: "10px", border: "none", paddingLeft: "5px" }}
+          ></input>
+          <select
+            name="type"
+            value={inputForm.type}
+            onChange={handleChange}
+            style={{ height: "40px", borderRadius: "10px", border: "none", paddingLeft: "2px" }}
+          >
+            <option value="person">Person</option>
+            <option value="company">Company</option>
+          </select>
+          <button
+            style={{ background: "white", color: "black" }}
+            onClick={handleSubmit}
+            type="submit"
+          >
+            Add
+          </button>
+        </form>
+      </div>
+      <hr></hr>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "400px",
+          height: "300px",
+          boxSizing: "border-box",
+          padding: "10px",
+        }}
+      >
+        <div style={{ display: "flex", gap: "10px" }}>
+          <button
+            style={
+              typeDisplay === "person"
+                ? { background: "white", color: "black" }
+                : { background: "black" }
+            }
+            onClick={() => setTypeDisplay("person")}
+          >
+            Person
+          </button>
+          <button
+            style={
+              typeDisplay === "company"
+                ? { background: "white", color: "black" }
+                : { background: "black" }
+            }
+            onClick={() => setTypeDisplay("company")}
+          >
+            Company
+          </button>
+          <button
+            onClick={handleAlphabetic}
+            style={
+              isSorted
+                ? { marginLeft: "auto", background: "white", color: "black" }
+                : { marginLeft: "auto", background: "black" }
+            }
+          >
+            A-Z
+          </button>
+        </div>
+        <hr style={{ margin: "10px 0" }}></hr>
+        <div style={{ display: "flex", flexDirection: "column", gap: "5px", overflowY: "auto" }}>
+          {typeDisplay === "person" ? (
+            <>
+              {isSorted
+                ? sortedPersonArray.map((c) => (
+                    <div key={c.number}>
+                      <Card name={c.name} number={c.number} handleDelete={handleDelete} />
+                    </div>
+                  ))
+                : filteredPerson.map((c) => (
+                    <div key={c.number}>
+                      <Card name={c.name} number={c.number} handleDelete={handleDelete} />
+                    </div>
+                  ))}
+            </>
+          ) : (
+            <>
+              {isSorted
+                ? sortedCompanyArray.map((c) => (
+                    <div key={c.number}>
+                      <Card name={c.name} number={c.number} handleDelete={handleDelete} />
+                    </div>
+                  ))
+                : filteredCompany.map((c) => (
+                    <div key={c.number}>
+                      <Card name={c.name} number={c.number} handleDelete={handleDelete} />
+                    </div>
+                  ))}
+            </>
+          )}
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default InxelTest;
+
+function Card(props) {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "50px",
+        background: "black",
+        display: "flex",
+        flexDirection: "row",
+        borderRadius: "10px",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
+          margin: "0 20px",
+        }}
+      >
+        <h3>{props.name}</h3>
+        <h3>{props.number}</h3>
+        <button onClick={() => props.handleDelete(props.number)} style={{ marginLeft: "auto" }}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    screens: {
+      ts: "601px",
+      ds: "1181px",
+    },
+  },
+  plugins: [],
+};
