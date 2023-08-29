55391314ffc1df1e98b05dd6c765cea16062db73
kinxori <116584982+kinxori@users.noreply.github.com>
2023-08-28 22:40:18 -0600

    moving contact manager component inside a div for responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,19 @@ export default function App() {
     <article className="w-[80%] mx-auto mt-20 font-[Rubik] ts:w-[70%] ds:w-[50%] ">
       <ScrollTopButton />
       <div className=" flex flex-col">
-        <h1 className=" text-[54px] font-bold underline   ">Hey There!</h1>
-        <h3 className="text-[24px] underline leading-[24px] font-bold my-5  ">
-          Welcome to this repository.
-        </h3>
-        <p className="font-sans leading-[18px]    ">
-          Here you can have a look on 1 technical challenge I completed for one company. You can have a
-          clear step by step view through the whole process to create this Contact Manager.
-        </p>
+        <div className=" flex flex-col">
+          <h1 className=" text-[54px] font-bold underline   ">Hey There!</h1>
+          <h3 className="text-[24px] underline leading-[24px] font-bold my-5  ">
+            Welcome to this repository.
+          </h3>
+          <p className="font-sans leading-[18px]    ">
+            Here you can have a look on 1 technical challenge I completed for one company. You can
+            have a clear step by step view through the whole process to create this Contact Manager.
+          </p>
+        </div>
+        <InxelTest />
       </div>
-      <InxelTest />
+
       <div className="flex flex-col gap-3 ">
         <p>
           This is running in a local enviorment with hard-coded data. This was a project requirment
